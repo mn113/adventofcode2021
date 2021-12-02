16b7dc755a4b948cc34b53dd9e222f71a4a21c68
Martin Nicholson <recyclebing+github@gmail.com>
2021-12-02 21:56:30 +0100

    Reformat Elixir with 2 spaces

--- elixir/01.exs
+++ elixir/01.exs
@@ -1,41 +1,43 @@
 defmodule Day01 do
-    defp read_input do
-        File.read!(Path.expand("../inputs/input01.txt"))
-        |> String.split("\n")
-        |> Enum.map(fn line -> line |> Integer.parse end)
-        |> Enum.map(fn {num, _} -> num end)
-    end
+  defp read_input do
+    File.read!(Path.expand("../inputs/input01.txt"))
+    |> String.split("\n")
+    |> Enum.map(fn line -> line |> Integer.parse() end)
+    |> Enum.map(fn {num, _} -> num end)
+  end
 
-    @doc """
-    Count the number of increasing pairs in the input list
-    """
-    def part1 do
-        input = read_input()
-        input
-        |> Enum.drop(1)
-        |> Enum.with_index
-        |> Enum.filter(fn {_, i} ->
-            Enum.at(input, i) < Enum.at(input, i+1)
-        end)
-        |> length
-        |> IO.inspect
-    end
+  @doc """
+  Count the number of increasing pairs in the input list
+  """
+  def part1 do
+    input = read_input()
 
-    @doc """
-    Count the number of times the sum of 3 consecutive elements increases in the input list
-    """
-    def part2 do
-        input = read_input()
-        input
-        |> Enum.drop(3)
-        |> Enum.with_index
-        |> Enum.filter(fn {_, i} ->
-            # items i+1 & i+2 would be equal on each side of comparison
-            Enum.at(input, i) < Enum.at(input, i+3)
-        end)
-        |> length
-        |> IO.inspect
-    end
+    input
+    |> Enum.drop(1)
+    |> Enum.with_index()
+    |> Enum.filter(fn {_, i} ->
+      Enum.at(input, i) < Enum.at(input, i + 1)
+    end)
+    |> length
+    |> IO.inspect()
+  end
+
+  @doc """
+  Count the number of times the sum of 3 consecutive elements increases in the input list
+  """
+  def part2 do
+    input = read_input()
+
+    input
+    |> Enum.drop(3)
+    |> Enum.with_index()
+    |> Enum.filter(fn {_, i} ->
+      # items i+1 & i+2 would be equal on each side of comparison
+      Enum.at(input, i) < Enum.at(input, i + 3)
+    end)
+    |> length
+    |> IO.inspect()
+  end
 end
 
 # P1: 1559
--- elixir/02.exs
+++ elixir/02.exs
@@ -1,55 +1,61 @@
 defmodule Day02 do
-    defp read_input do
-        File.read!(Path.expand("../inputs/input02.txt"))
-        |> String.split("\n")
-        |> Enum.map(fn line ->
-            [term, digits] = String.split(line)
-            {String.to_atom(term), elem(Integer.parse(digits), 0)}
-        end)
-    end
+  defp read_input do
+    File.read!(Path.expand("../inputs/input02.txt"))
+    |> String.split("\n")
+    |> Enum.map(fn line ->
+      [term, digits] = String.split(line)
+      {String.to_atom(term), elem(Integer.parse(digits), 0)}
+    end)
+  end
 
-    @doc """
-    Determine the area travelled by the sub (forward * down)
-    """
-    def part1 do
-        initial = %{ forward: 0, up: 0, down: 0 }
+  @doc """
+  Determine the area travelled by the sub (forward * down)
+  """
+  def part1 do
+    initial = %{forward: 0, up: 0, down: 0}
 
-        collected = read_input()
-        |> Enum.reduce(
-            initial,
-            fn {dir, x}, acc ->
-                Map.update!(acc, dir, &(&1 + x))
-            end
-        )
+    collected =
+      read_input()
+      |> Enum.reduce(
+        initial,
+        fn {dir, x}, acc ->
+          Map.update!(acc, dir, &(&1 + x))
+        end
+      )
 
-        collected.forward * abs(collected.down - collected.up)
-        |> IO.inspect(label: "P1")
-    end
+    (collected.forward * abs(collected.down - collected.up))
+    |> IO.inspect(label: "P1")
+  end
 
-    @doc """
-    Determine the new area travelled by the sub (forward * depth)
-    """
-    def part2 do
-        initial = %{ forward: 0, depth: 0, aim: 0 }
+  @doc """
+  Determine the new area travelled by the sub (forward * depth)
+  """
+  def part2 do
+    initial = %{forward: 0, depth: 0, aim: 0}
 
-        collected = read_input()
-        |> Enum.reduce(
-            initial,
-            fn {dir, x}, acc ->
-                case dir do
-                    :down -> acc |> Map.update!(:aim, &(&1 + x))
-                    :up -> acc |> Map.update!(:aim, &(&1 - x))
-                    :forward ->
-                        acc
-                        |> Map.update!(:forward, &(&1 + x))
-                        |> Map.update!(:depth, &(&1 + x * acc.aim))
-                end
-            end
-        )
+    collected =
+      read_input()
+      |> Enum.reduce(
+        initial,
+        fn {dir, x}, acc ->
+          case dir do
+            :down ->
+              acc |> Map.update!(:aim, &(&1 + x))
 
-        collected.forward * collected.depth
-        |> IO.inspect(label: "P2")
-    end
+            :up ->
+              acc |> Map.update!(:aim, &(&1 - x))
+
+            :forward ->
+              acc
+              |> Map.update!(:forward, &(&1 + x))
+              |> Map.update!(:depth, &(&1 + x * acc.aim))
+          end
+        end
+      )
+
+    (collected.forward * collected.depth)
+    |> IO.inspect(label: "P2")
+  end
 end
 
 # P1: 1451208
