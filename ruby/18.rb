47f10ce2c177f97ef7f88c01b74caa7c2eaf0f43
Martin Nicholson <recyclebing+github@gmail.com>
2021-12-20 23:48:00 +0100

    Day 18 improve docs

--- ruby/18.rb
+++ ruby/18.rb
@@ -2,16 +2,6 @@
 
 def read_input
     File.open("../inputs/input18.txt", "r").each_line.map{ |l| l.chomp }
-# """[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
-# [7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
-# [[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
-# [[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
-# [7,[5,[[3,8],[1,4]]]]
-# [[2,[2,2]],[8,[8,1]]]
-# [2,9]
-# [1,[[[9,3],9],[[9,0],[0,7]]]]
-# [[[5,[7,4]],7],1]
-# [[[[4,2],2],6],[8,7]]"""
 end
 
 # Replace portion of string at a given index/length with replacement
@@ -23,7 +13,7 @@ def replace(str, idx, len, replacement)
     str.sub!(tmp, '')
 end
 
-# first [] pair nested more than 4 levels deep
+# Find first [?,?] pair nested more than 4 levels deep
 def first_explode_index(str)
     idx = nil
     depth = 0
@@ -43,17 +33,17 @@ def first_explode_index(str)
 end
 
 # Deeply nested pair [m,n] must be removed.
-# - m added to nearest left neighbour (or removed)
-# - n added to nearest right neighbour (or removed)
+# - m added to nearest left neighbour ww (or removed)
+# - n added to nearest right neighbour zz (or removed)
 def explode_at(str, idx)
-    # str: 1 ... [2,3] ... 14
-    # ids: w ... xm,ny ... zz
+    # str: 01 ... [2,3] ... 14
+    # ids: ww ... xm,ny ... zz
     max_id = str.length - 1
     idy = idx
     idy += 1 until str[idy] == ']'
     #p "explode at #{idx} : #{str[idx..idy]}"
     (m,n) = eval(str[idx..idy]) # match [1,2] or [12,13]
-    # seek left from x -> w may be 1 or 2 digits
+    # seek left from bracket x to find number w (may be 1 or 2 digits)
     idw = idx
     idw -= 1 until str[idw] =~ /\d/ or idw == 0 # 1st char
     idw -= 1 if idw > 0 and str[idw-1] =~ /\d/ # 2nd char
@@ -61,7 +51,7 @@ def explode_at(str, idx)
     w += str[idw+1] if str[idw+1] =~ /\d/
     wlen = w.length # 1 or 2
     w = w.to_i
-    # seek right from y -> z may be 1 or 2 digits
+    # seek right from bracket y to find number z (may be 1 or 2 digits)
     idz = idy
     idz += 1 until str[idz] =~ /\d/ or idz == max_id
     z = str[idz]
@@ -69,7 +59,7 @@ def explode_at(str, idx)
     zlen = z.length # 1 or 2
     z = z.to_i
 
-    # move numerics and delete exploded pair - replacements can change length so must be done from back to front
+    # update numerics and delete exploded pair x..y - replacements can modify length so must be done from last to first
     if idz < max_id
         str = replace(str, idz, zlen, (z + n).to_s)
     end
@@ -83,18 +73,15 @@ end
 # Multi-digit number gets halved and rounded down/up - becomes [m,n]
 def split_at(str, idx)
     num = str[idx,2].to_i # always 2-digit?
-    #p "split at #{idx} : #{num}"
     m = (num / 2).floor
     n = num - m
-    # p ['spl', num, m, n]
     str = replace(str, idx, 2, "[#{m},#{n}]")
 end
 
 # Continuously explode or split first matching term, until no terms match
 def simplify(str)
-    while 1 do
+    while true do
         explode_idx = first_explode_index(str)
-        # first number >= 10
         split_idx = str.index(/\d\d/)
 
         if explode_idx
@@ -118,7 +105,7 @@ end
 
 # Convert number list to integer from inside out according to rules
 def magnitude(str)
-    while str.start_with?('[')
+    while str.start_with?('[') do
         str.gsub!(/\[(\d+),(\d+)\]/) do |_|
             (($1.to_i * 3) + ($2.to_i * 2)).to_s
         end
@@ -130,7 +117,7 @@ input = read_input()
 
 p "Part 1: #{magnitude(sum_list(input))}" # 4289
 
-# Maximum magnitude of 2 numbers: x+y or y+x
+# Maximum magnitude of 2 number sum: A + B or B + A
 max = 0
 input.permutation(2).each do |pair|
     magn = magnitude(sum_list(pair))
